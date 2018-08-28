1e7a782b40ff7ee14384905a74918e40704ed866
Steven <stevenhuynh17@gmail.com>
2018-08-27 23:08:50 -0700

    bare components added

---
+++ components/DeckList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class DeckList extends Component {
+  render() {
+    return(
+      <View>
+        <Text>
+          DeckList
+        </Text>
+      </View>
+    )
+  }
+}
---
+++ components/SingleDeck.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+export default class SingleDeck extends Component {
+  render() {
+    return(
+      <View>
+        <Text>Single Deck</Text>
+      </View>
+    )
+  }
+}
